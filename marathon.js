fba7afa620a403d4ae2cd8f8d8af50e9e0fcf07e
Carlos Herrera <chgusy@gmail.com>
2012-06-04 21:34:07 -0400

    axis

--- marathon.js
+++ marathon.js
@@ -9,26 +9,21 @@ y = d3.scale.linear().domain([endDist, startDist]).range([0+margin, h-margin]),
 x = d3.scale.linear().domain([startTime, endTime]).range([0+margin-5, w]),
 time = d3.range(startTime, endTime);
 
-var vis = d3.select("#vis")
+var svg = d3.select("#vis")
 	.append("svg:svg")
 	.attr("width",w)
 	.attr("height",h)
 	.append("svg:g")
 
-var line = d3.svg.line()
-	.x(function(d,i) {return x(d.x);})
-	.y(function(d) { return y(d.y);})
-
-vis.append("svg:line")
-	.attr("x1",x(startTime))
-	.attr("y1",y(startDist))
-	.attr("x2",x(startTime))
-	.attr("y2",y(endDist))
-	.attr("class","axis")
-
-vis.append("svg:line")
-    .attr("x1", x(startTime))
-    .attr("y1", y(startDist))
-    .attr("x2", x(endTime))
-    .attr("y2", y(startDist))
-    .attr("class", "axis")
+xAxis = d3.svg.axis().scale(x)
+yAxis = d3.svg.axis().scale(y).orient("left")
+v=280;
+svg.append("svg:g")
+	.attr("class", "xaxis")
+	.attr("transform","translate(0,"+(h-margin)+")")
+        .call(xAxis)
+                
+svg.append("svg:g")
+	.attr("class", "yaxis")
+        .attr("transform", "translate(30,0)")
+        .call(yAxis)
